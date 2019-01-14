2ac4c1fa849c5350116ec5e1151b70947c502f19
Luke Kersting <lkersting@wisc.edu>
2019-01-14 11:27:05 -0600

    updated critcal line energies for the low density problem.

--- electron/self_adjoint/brem_only/plot_results.py
+++ electron/self_adjoint/brem_only/plot_results.py
@@ -177,7 +177,7 @@ ax0.grid(linestyle=':')
 ax1.grid(linestyle=':')
 
 plt.xlim(0.0085,0.01)
-plt.ylim(0.1,2.0)
+plt.ylim(0.85,1.15)
 
 # remove vertical gap between subplots
 plt.subplots_adjust(hspace=.0)
