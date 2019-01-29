13915daadaf8eeea8f84a4aa14d11305e4dfa5b5
Luke Kersting <lkersting@wisc.edu>
2019-01-29 09:04:01 -0600

    updated bins again.

--- electron/self_adjoint/H/0.01_uniform/adjoint.py
+++ electron/self_adjoint/H/0.01_uniform/adjoint.py
@@ -101,7 +101,7 @@ def runSimulation( threads, histories, time ):
   event_handler = Event.EventHandler( properties )
 
   # Set the energy bins
-  bins = list(Utility.doubleArrayFromString( "{ 1e-4, 399l, 1e-2}" ))
+  bins = list(Utility.doubleArrayFromString( "{ 1e-4, 99l, 8e-3, 149l 1e-2}" ))
 
   ## ------------------------ Surface Flux Estimator ------------------------ ##
 
